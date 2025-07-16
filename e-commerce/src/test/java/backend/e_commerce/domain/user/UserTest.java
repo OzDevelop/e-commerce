db608c716b34986a20b0361fdc4355ccb2d503d0
byunsp3610@gmail.com <byunsp3610@gmail.com>
2025-07-16 22:37:53 +0900

    test: User Domain 생성에 따른 테스트 수정
    Domain의 불변성 유지를 위해 테스트 검증 방식 수정

--- e-commerce/src/test/java/backend/e_commerce/domain/user/UserTest.java
+++ e-commerce/src/test/java/backend/e_commerce/domain/user/UserTest.java
@@ -14,41 +14,40 @@ class UserTest {
         String password = "password";
         String name = "홍길동";
         String phone = "010-1234-5678";
-        User.Role role = User.Role.USER;
+        UserRole role = UserRole.USER;
 
-        user = User.create(email, password, name, phone, role);
+        user = User.createUser(email, password, name, phone, role);
     }
 
     @Test
     void 사용자_생성_테스트() {
-
         // then
         assertEquals("test@example.com", user.getEmail());
         assertEquals("password", user.getPassword());
         assertEquals("홍길동", user.getName());
         assertEquals("010-1234-5678", user.getPhone());
-        assertEquals(User.Role.USER, user.getRole());
+        assertEquals(UserRole.USER, user.getRole());
 
     }
 
     @Test
     void 사용자프로필_수정_테스트() {
         // when
-        user.changeUserInfo("영희", "010-2222-1111");
+        User updatedUser = user.changeUserInfo("영희", "010-2222-1111");
 
         // then
-        assertEquals("영희", user.getName());
-        assertEquals("010-2222-1111", user.getPhone());
+        assertEquals("영희", updatedUser.getName());
+        assertEquals("010-2222-1111", updatedUser.getPhone());
     }
 
 
     @Test
     void 비밀번호_변경_테스트() {
         // when
-        user.changePassword("newPassword");
+        User updatedUser = user.changePassword("newPassword");
 
         // then
-        assertEquals("newPassword", user.getPassword());
+        assertEquals("newPassword", updatedUser.getPassword());
     }
 
 }
