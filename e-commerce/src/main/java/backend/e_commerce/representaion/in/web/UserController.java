ebc0afa7a49c0c3682ff784425243e51a22ddd7d
byunsp3610@gmail.com <byunsp3610@gmail.com>
2025-07-17 22:16:51 +0900

    Feat: User Controller(유저 등록) 및 Dto 생성

---
+++ e-commerce/src/main/java/backend/e_commerce/representaion/in/web/UserController.java
@@ -0,0 +1,28 @@
+package backend.e_commerce.representaion.in.web;
+
+import backend.e_commerce.application.command.user.RegisterUserCommand;
+import backend.e_commerce.application.port.in.user.UserInfoCommandUserUseCase;
+import backend.e_commerce.representaion.request.RegisterUserRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserInfoCommandUserUseCase userInfoCommandUserUseCase;
+
+    @PostMapping("/register")
+    //TODO - 공통 Response 객체 구현 시 변경 예정
+    public ResponseEntity<Void> registerUser(@RequestBody RegisterUserRequestDto request) {
+        RegisterUserCommand command = request.toRegisterUserCommand();
+        userInfoCommandUserUseCase.registerUser(command);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
---
+++ e-commerce/src/main/java/backend/e_commerce/representaion/request/RegisterUserRequestDto.java
@@ -0,0 +1,31 @@
+package backend.e_commerce.representaion.request;
+
+import backend.e_commerce.application.command.user.RegisterUserCommand;
+import backend.e_commerce.application.command.user.RegisterUserCommand.AddressCommand;
+import backend.e_commerce.domain.user.UserRole;
+import java.util.List;
+
+public record RegisterUserRequestDto (
+    String email,
+    String password,
+    String name,
+    String phone,
+    String role,
+    List<AddressRequestDto> addresses
+) {
+    public RegisterUserCommand toRegisterUserCommand() {
+        List<AddressCommand> addressCommands = addresses.stream()
+                .map(AddressRequestDto::toCommand)
+                .toList();
+
+        return new RegisterUserCommand(
+                email,
+                password,
+                name,
+                phone,
+                UserRole.valueOf(role.toUpperCase()),
+                addressCommands
+
+        );
+    }
+}
