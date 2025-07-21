aab3951d41baadc99b69b2e8f7fc414d6696ddc5
byunsp3610@gmail.com <byunsp3610@gmail.com>
2025-07-21 20:52:29 +0900

    feat: Payment 필드 생성
    Order 테스트를 위해 필드 우선 작성

---
+++ e-commerce/src/main/java/backend/e_commerce/domain/payment/PaymentMethod.java
@@ -0,0 +1,14 @@
+package backend.e_commerce.domain.payment;
+
+public enum PaymentMethod {
+    CREDIT_CARD("신용 카드"),
+    DEBIT_CARD("직불 카드"),
+    PAYPAL("페이팔"),
+    BANK_TRANSFER("계좌 이체");
+
+    final String desc;
+
+    PaymentMethod(String desc) {
+        this.desc = desc;
+    }
+}
