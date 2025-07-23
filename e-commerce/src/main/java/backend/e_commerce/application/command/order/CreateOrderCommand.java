5a319a088de75c1710abaf213a5d9299defdcef3
byunsp3610@gmail.com <byunsp3610@gmail.com>
2025-07-23 22:33:47 +0900

    feat: Order Service 관련 UseCase, Command, Mapper

---
+++ e-commerce/src/main/java/backend/e_commerce/application/command/order/CreateOrderCommand.java
@@ -0,0 +1,37 @@
+package backend.e_commerce.application.command.order;
+
+import backend.e_commerce.domain.order.OrderItem;
+import backend.e_commerce.domain.payment.Payment;
+import backend.e_commerce.domain.user.Address;
+import backend.e_commerce.representaion.request.CreateOrderRequestDto;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class CreateOrderCommand {
+    private final Long userId;
+    private final Address address;
+    private final List<OrderItem> orderItems;
+    private final Payment payment;
+
+    public static CreateOrderCommand toCommand(CreateOrderRequestDto dto) {
+        List<OrderItem> orderItems = dto.getOrderItems().stream()
+                .map(item -> OrderItem.builder()
+                        .productId(item.getProductId())  // productId 타입 맞추기 주의 (String or Long)
+                        .quantity(item.getQuantity())
+                        .amount(item.getUnitPrice())
+                        .build())
+                .toList();
+
+        return CreateOrderCommand.builder()
+                .userId(dto.getUserId())
+                .address(dto.getAddress())
+                .orderItems(orderItems)
+                .payment(dto.getPayment())
+                .build();
+    }
+}
