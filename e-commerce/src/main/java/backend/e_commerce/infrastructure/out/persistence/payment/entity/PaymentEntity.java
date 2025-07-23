85956c377001a789d627553277d01ea922b2250a
byunsp3610@gmail.com <byunsp3610@gmail.com>
2025-07-23 22:36:05 +0900

    feat: Payment Entity, Mapper

---
+++ e-commerce/src/main/java/backend/e_commerce/infrastructure/out/persistence/payment/entity/PaymentEntity.java
@@ -0,0 +1,38 @@
+package backend.e_commerce.infrastructure.out.persistence.payment.entity;
+
+import backend.e_commerce.domain.payment.PaymentMethod;
+import backend.e_commerce.domain.payment.PaymentStatus;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "payments")
+public class PaymentEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String paymentKey;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentMethod paymentMethod;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus paymentStatus;
+
+    private int totalAmount;
+    private int canceledAmount;
+}
