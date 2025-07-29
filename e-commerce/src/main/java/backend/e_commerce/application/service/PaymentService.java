a402b04f155efc0fa99ff8ba980492a85b34f074
byunsp3610@gmail.com <byunsp3610@gmail.com>
2025-07-29 22:04:56 +0900

    feat, refactor: payment service, controller 수정, 추가

--- e-commerce/src/main/java/backend/e_commerce/application/service/PaymentService.java
+++ e-commerce/src/main/java/backend/e_commerce/application/service/PaymentService.java
@@ -3,44 +3,62 @@ package backend.e_commerce.application.service;
 import backend.e_commerce.application.command.payment.PaymentApprovedCommand;
 import backend.e_commerce.application.command.payment.PaymentCancelledCommand;
 import backend.e_commerce.application.port.in.payment.PaymentCommandUseCase;
+import backend.e_commerce.application.port.in.payment.PaymentQueryUseCase;
 import backend.e_commerce.application.port.out.OrderRepository;
 import backend.e_commerce.application.port.out.PaymentLedgerRepository;
 import backend.e_commerce.application.port.out.api.PaymentAPIs;
 import backend.e_commerce.domain.order.Order;
 import backend.e_commerce.domain.order.OrderStatus;
+import backend.e_commerce.domain.payment.PaymentLedger;
+import backend.e_commerce.infrastructure.out.pg.toss.response.PaymentCancelResponseDto;
 import backend.e_commerce.infrastructure.out.pg.toss.response.PaymentConfirmResponseDto;
-import backend.e_commerce.representaion.request.payment.PaymentConfirmRequestDto;
+import backend.e_commerce.representaion.request.payment.PaymentCancelRequestDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class PaymentService implements PaymentCommandUseCase {
+public class PaymentService implements PaymentCommandUseCase, PaymentQueryUseCase {
     private final PaymentAPIs tossPayment;
+
+    private final OrderService orderService;
 
     private final OrderRepository orderRepository;
     private final PaymentLedgerRepository paymentLedgerRepository;
 
 
     @Override
+    @Transactional
     public String paymentApproved(PaymentApprovedCommand command) {
         ///  1. 주문이 완료되었는지 확인 (status가 complete 인지)
         isCompletedOrder(UUID.fromString(command.getOrderId()));
         ///  2. PG사로 결제 승인 요청 보내기
         PaymentConfirmResponseDto responseDto = tossPayment.requestPaymentConfirm(command.toPaymentConfirmRequestDto());
 
-        ///  3. 승인이 되었다면
-        if (tossPayment.isPaymentConfirmed(responseDto.getPaymentStatus())) {
+        System.out.println("responseDto.getOrderId():"+responseDto.getOrderId());
+        System.out.println("responseDto.getPaymentStatus():"+responseDto.getStatus());
+
+        ///  3. 승인이 되었다면 ( 그 전에 PaymentStatus가 Done으로 변경되어야 함.)
+        if (tossPayment.isPaymentConfirmed(responseDto.getStatus())) {
         ///  3-2. 결제원장(ledger) 저장, 주문상태 변경 -> 결제 완료
         Order completedOrder = orderRepository.findById(UUID.fromString(responseDto.getOrderId()))
                 .orElseThrow();
         completedOrder.completeOrder();
+            log.warn("여기까지 오늬??????");
+            try {
+                System.out.println("응답 전체: " + new ObjectMapper().writeValueAsString(responseDto));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
-        // 결제원장 저장
-        paymentLedgerRepository.save(responseDto.toPaymentLedgerEntity());
+
+            // 결제원장 저장
+        paymentLedgerRepository.save(responseDto.toPaymentLedgerDomain());
 
             /**
              * Order 상태 수정 저장,
@@ -48,22 +66,47 @@ public class PaymentService implements PaymentCommandUseCase {
              * Ledger 생성,
              */
 
+            return "success";
         }
 
-        return "";
+        return "fail";
     }
 
     @Override
-    public String paymentCancel(PaymentCancelledCommand command) {
-        return "";
+    public boolean paymentCancel(PaymentCancelledCommand command) {
+        String paymentKey = command.getPaymentKey();
+
+        Order wantedCancelOrder = orderService.getOrderInfo(command.getOrderId());
+        PaymentLedger paymentLedger = getLastPaymentLedger(paymentKey);
+        if(!(wantedCancelOrder.getOrderStatus() == OrderStatus.COMPLETED)) {
+            PaymentCancelRequestDto requestDto = new PaymentCancelRequestDto(command.getCancelReason(), command.getCancellationAmount());
+
+            PaymentCancelResponseDto responseDto = tossPayment.requestPaymentCancel(paymentKey, requestDto);
+
+            paymentLedgerRepository.save(responseDto.toPaymentLedgerDomain());
+
+            if(command.getItemIds() != null || command.getItemIds().length > 0 ) {
+                wantedCancelOrder.orderCancel(command.getItemIds());
+            } else {
+                wantedCancelOrder.orderAllCancel();
+            }
+            return true;
+
+        }
+        return false;
     }
 
     private void isCompletedOrder(UUID orderId) {
         Order order = orderRepository.findById(orderId).orElseThrow();
         OrderStatus status = order.getOrderStatus();
 
-        if (!status.equals(OrderStatus.COMPLETED)) {
+        if (!status.equals(OrderStatus.PENDING_PAYMENT)) {
             throw new IllegalArgumentException("주문이 완료되지 않았거나, 결제 준비가 되지 않음");
         }
+    }
+
+    @Override
+    public PaymentLedger getLastPaymentLedger(String paymentKey) {
+        return paymentLedgerRepository.findOneByPaymentKeyDesc(paymentKey);
     }
 }
--- e-commerce/src/main/java/backend/e_commerce/representaion/in/web/PaymentController.java
+++ e-commerce/src/main/java/backend/e_commerce/representaion/in/web/PaymentController.java
@@ -1,17 +1,23 @@
 package backend.e_commerce.representaion.in.web;
 
+import backend.e_commerce.application.command.payment.PaymentApprovedCommand;
+import backend.e_commerce.application.port.in.payment.PaymentCommandUseCase;
 import backend.e_commerce.infrastructure.out.pg.toss.TossPayment;
+import backend.e_commerce.representaion.request.payment.PaymentConfirmRequestDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
 @Slf4j
 public class PaymentController {
-    private final TossPayment tossPayment;
+
+    private final PaymentCommandUseCase paymentCommandUseCase;
 
     @GetMapping("/success")
     public String paymentConfirm(
@@ -28,5 +34,17 @@ public class PaymentController {
         return "fail";
     }
 
+    @PostMapping("/confirm")
+    public String paymentConfirm(@RequestBody PaymentConfirmRequestDto requestDto) {
+        PaymentApprovedCommand command = new PaymentApprovedCommand(
+                requestDto.getPaymentKey(),
+                requestDto.getOrderId(),
+                requestDto.getAmount()
+        );
+        log.warn("여기까지 오늬?");
+        log.warn(command.getOrderId());
 
+
+        return paymentCommandUseCase.paymentApproved(command);
+    }
 }
