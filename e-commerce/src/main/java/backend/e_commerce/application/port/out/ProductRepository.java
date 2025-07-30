ceefbc7b9dc029ac13bf06672633dd3546b1b354
byunsp3610@gmail.com <byunsp3610@gmail.com>
2025-07-30 19:21:17 +0900

    refactor: ProductServive JPA 연동

--- e-commerce/src/main/java/backend/e_commerce/application/port/out/ProductRepository.java
+++ e-commerce/src/main/java/backend/e_commerce/application/port/out/ProductRepository.java
@@ -1,23 +1,16 @@
 package backend.e_commerce.application.port.out;
 
 import backend.e_commerce.domain.product.Product;
+import backend.e_commerce.infrastructure.out.persistence.product.entity.ProductEntity;
 import java.util.List;
 import java.util.Optional;
 
 public interface ProductRepository {
 
-    // 등록
     Product save(Product product);
-
-    // 다건 등록
     List<Product> saveAll(List<Product> products);
-
-    // 단건 조회
     Optional<Product> findById(Long productId);
 
-    // 삭제
     void deleteById(Long productId);
-
-    // 존재 여부 확인
     boolean existsById(Long productId);
 }
