644adf8764ae721d47e62ac7ae043a66f25580eb
byunsp3610@gmail.com <byunsp3610@gmail.com>
2025-08-07 22:57:47 +0900

    refactor: ComponentScan 범위 수정

--- e-commerce/src/main/java/backend/e_commerce/ECommerceApplication.java
+++ e-commerce/src/main/java/backend/e_commerce/ECommerceApplication.java
@@ -2,10 +2,12 @@ package backend.e_commerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@ComponentScan(basePackages = "backend")
 public class ECommerceApplication {
 
 	public static void main(String[] args) {
