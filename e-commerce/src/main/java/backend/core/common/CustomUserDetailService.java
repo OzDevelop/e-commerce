9ef54ebea84ec1f5a2e47c3eefcafcdf992ea12e
byunsp3610@gmail.com <byunsp3610@gmail.com>
2025-08-07 22:56:51 +0900

    Feat: 인증 정보 관리 , Securit Config 생성

---
+++ e-commerce/src/main/java/backend/core/common/CustomUserDetailService.java
@@ -0,0 +1,35 @@
+package backend.core.common;
+
+import backend.e_commerce.application.port.out.UserRepository;
+import backend.e_commerce.domain.user.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+    private final UserRepository userRepository;
+//    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        try {
+            User user = userRepository.findByEmail(email);
+            System.out.println("000000000000000000000000000000000000000000000");
+            System.out.println("user.getEmail "+user.getEmail());
+            System.out.println("user.getEmail "+user.getPassword());
+            System.out.println("000000000000000000000000000000000000000000000");
+            return createUserDetails(user);
+        } catch (IllegalArgumentException e) {
+            throw new UsernameNotFoundException("해당 사용자를 찾을 수 없습니다: " + email);
+        }
+    }
+
+    private UserDetails createUserDetails(User user) {
+        return new CustomUserDetails(user);
+    }
+}
---
+++ e-commerce/src/main/java/backend/core/common/CustomUserDetails.java
@@ -0,0 +1,54 @@
+package backend.core.common;
+
+import backend.e_commerce.domain.user.User;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+
+public class CustomUserDetails implements UserDetails {
+    private final User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(() -> user.getUserRole());
+    }
+
+    @Override
+    public String getPassword() {
+        System.out.println("CustomUserDtails.getPassword>>" + user.getPassword());
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
