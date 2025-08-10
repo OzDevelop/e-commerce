d7aff5e9125359ddf222911cdd588eb76d4f393f
byunsp3610@gmail.com <byunsp3610@gmail.com>
2025-08-10 23:28:32 +0900

    feat: CustomDeliveryException 적용

--- e-commerce/src/main/java/backend/core/common/errorcode/execption/DeliveryException.java
+++ e-commerce/src/main/java/backend/core/common/errorcode/execption/DeliveryException.java
@@ -1,9 +1,16 @@
 package backend.core.common.errorcode.execption;
 
+import backend.core.common.errorcode.errorcode.DeliveryErrorCode;
 import backend.core.common.errorcode.errorcode.UserErrorCode;
+import java.util.Map;
 
-public class DeliveryException extends RestApiException{
-    public DeliveryException(UserErrorCode errorCode) {
-        super(errorCode);
+public class DeliveryException extends RestApiException {
+
+    public DeliveryException(DeliveryErrorCode errorCode) {
+        super(errorCode, null);
+    }
+
+    public DeliveryException(DeliveryErrorCode errorCode, Map<String, Object> details) {
+        super(errorCode, details);
     }
 }
