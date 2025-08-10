da25ca4a2c81117bd6878b8168a673d8377659f8
byunsp3610@gmail.com <byunsp3610@gmail.com>
2025-08-10 22:37:50 +0900

    feat: CustomUserException 적용

--- e-commerce/src/main/java/backend/core/common/errorcode/errorcode/UserErrorCode.java
+++ e-commerce/src/main/java/backend/core/common/errorcode/errorcode/UserErrorCode.java
@@ -7,8 +7,17 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum UserErrorCode implements ErrorCode {
+    // User
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
-    DUPLICATE_EMAIL(HttpStatus.BAD_REQUEST, "이미 존재하는 이메일입니다.");
+    DUPLICATE_EMAIL(HttpStatus.BAD_REQUEST, "이미 존재하는 이메일입니다."),
+
+    // Address
+    ADDRESS_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 주소입니다."),
+
+    // Password
+    INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "잘못된 비밀번호입니다.")
+    ;
+
 
 
     private final HttpStatus httpStatus;
