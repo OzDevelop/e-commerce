b9b6eddb405c5ae2826be83fe08350076f0b1741
byunsp3610@gmail.com <byunsp3610@gmail.com>
2025-08-11 13:53:25 +0900

    fix: swagger 관련 요청은 원본 응답

--- e-commerce/src/main/java/backend/core/common/Response/ResponseWrapper.java
+++ e-commerce/src/main/java/backend/core/common/Response/ResponseWrapper.java
@@ -12,6 +12,19 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 public class ResponseWrapper implements ResponseBodyAdvice<Object> {
     @Override
     public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+        // 모든 응답에 감싸기를 적용하되, 다음 예외는 제외
+        Class<?> clazz = returnType.getParameterType();
+
+        // ApiResponse로 이미 감싸진 경우 다시 감싸지 않도록 예외 처리
+        if (ApiResponse.class.isAssignableFrom(clazz)) {
+            return false;
+        }
+
+        // byte[] 같은 바이너리 데이터는 감싸지 말기
+        if (byte[].class.isAssignableFrom(clazz)) {
+            return false;
+        }
+
         return true;
     }
 
@@ -23,6 +36,10 @@ public class ResponseWrapper implements ResponseBodyAdvice<Object> {
                                   ServerHttpRequest request, ServerHttpResponse response
     ) {
         String path = request.getURI().getPath();
+
+        if (path.startsWith("/v3/api-docs") || path.startsWith("/swagger-ui") || path.startsWith("/swagger-resources")) {
+            return body;
+        }
 
         if (body instanceof ErrorResponse) {
             return ApiResponse.builder()
