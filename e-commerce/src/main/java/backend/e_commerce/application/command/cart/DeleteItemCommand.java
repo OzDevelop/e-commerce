e5a537827377913c7aa110d5f4fef2d9c730d6f6
byunsp3610@gmail.com <byunsp3610@gmail.com>
2025-08-12 20:54:39 +0900

    chore: cart command에 Builder 추가

--- e-commerce/src/main/java/backend/e_commerce/application/command/cart/DeleteItemCommand.java
+++ e-commerce/src/main/java/backend/e_commerce/application/command/cart/DeleteItemCommand.java
@@ -1,10 +1,12 @@
 package backend.e_commerce.application.command.cart;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
+@Builder
 public class DeleteItemCommand {
     private final Long userId;
     private final Long cartItemId;
