738e7fe2865254c71ecf3907b35ad27bb389b019
byunsp3610@gmail.com <byunsp3610@gmail.com>
2025-08-13 15:15:07 +0900

    feat: CartRepo removeItems 추가

--- e-commerce/src/main/java/backend/e_commerce/infrastructure/out/persistence/cart/CartRepositoryImpl.java
+++ e-commerce/src/main/java/backend/e_commerce/infrastructure/out/persistence/cart/CartRepositoryImpl.java
@@ -2,7 +2,9 @@ package backend.e_commerce.infrastructure.out.persistence.cart;
 
 import backend.e_commerce.application.port.out.CartRepository;
 import backend.e_commerce.domain.cart.Cart;
+import backend.e_commerce.domain.cart.CartItem;
 import backend.e_commerce.infrastructure.out.persistence.cart.entity.CartEntity;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -23,5 +25,15 @@ public class CartRepositoryImpl implements CartRepository {
         return jpaCartRepository.findByUserId(userId)
                 .map(CartEntityMapper::fromCartEntityToCart)
                 .orElseThrow(() -> new IllegalArgumentException("Cart not found for userId: " + userId));
+    }
+
+    @Override
+    public void removeItems(Long userId, List<CartItem> items) {
+        CartEntity cartEntity = jpaCartRepository.findByUserId(userId)
+                .orElseThrow(() -> new IllegalStateException("Cart not found for userId: " + userId));
+
+        List<Long> removeIds = items.stream().map(CartItem::getId).toList();
+
+        cartEntity.getItems().removeIf(itemEntity -> removeIds.contains(itemEntity.getId()));
     }
 }
